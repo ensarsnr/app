14d5a2150230601ae18137b577a4f5abaef96ff0
NapolyonNereli <dev.anise@outlook.com>
2023-03-25 19:25:28 +0300

    add nameSlice

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,17 +2,23 @@ import { Paper, TextField } from '@mui/material'
 import { Button } from 'react-bootstrap'
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { LABEL_DEPARTMENT, LABEL_NAME, LOGIN_BUTTON, PLACEHOLDER_DEPARTMENT } from '../constants/constText'
+import { LABEL_DEPARTMENT, LABEL_NAME, LABEL_SURNAME, LOGIN_BUTTON, PLACEHOLDER_DEPARTMENT, PLACEHOLDER_NAME } from '../constants/constText'
 
 function Form() {
-
   return (
     <Paper elevation={1} className="mt-5 w-75">
-      <form style={{ padding: "100px 20px 100px 20px" }} className='text-center'>
+      <form style={{ padding: "5  0px 20px 50px 20px" }} className='text-center'>
         <div className='mb-4'>
           <TextField
             className='bg-light w-75'
             label={LABEL_NAME}
+            variant='outlined'
+          />
+        </div>
+        <div className='mb-4'>
+          <TextField
+            className='bg-light w-75'
+            label={LABEL_SURNAME}
             variant='outlined'
           />
         </div>
